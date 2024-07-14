3dcc35935e726b3f0e851ca1eb551ea0f8c95ced
Anthony <anton.domrachev.95@mail.ru>
2024-07-14 10:27:05 +0300

    deck-mode

--- deck.js
+++ deck.js
@@ -74,12 +74,87 @@ else{
 
 
 //Выведите в консоль все целые числа от 1 до 100.
-let arrHungred = []
+const arrHungred = []
 
 for(let i =1; i<=100; i++){
     arrHungred.push(i)
 }
 console.log(arrHungred)
+
+
+//Выведите в консоль все целые числа от -100 до 1
+const ab = []
+for(let i = -100; i<=0; i++){
+    ab.push(i)
+}
+console.log(ab)
+
+
+//Выведите в консоль все целые числа от 100 до 1.
+const ac = []
+for(let i = 100; i>=1; i--){
+    ac.push(i)
+}
+console.log(ac)
+
+
+//Выведите в консоль все четные числа из промежутка от 1 до 100.
+const ad = []
+for(let i = 1; i<=100; i++){
+    if(Math.ceil(i/2) === i/2){
+        ad.push(i)
+    }
+}
+console.log(ad)
+
+
+//Выведите в консоль все числа кратные трем в промежутке от 1 до 100.
+const af = []
+for(let i=1; i<=100; i++){
+    if(Math.ceil(i/3) === i/3){
+        af.push(i)
+    }
+}
+console.log(af)
+
+
+//Найдите сумму всех целых чисел от 1 до 100.
+let ah = 0
+for(let i = 1; i<=100; i++){
+    ah = ah + i
+}
+console.log(ah)
+
+
+//Найдите сумму всех целых четных чисел в промежутке от 1 до 100.
+let at = 0
+for(let i = 1; i<=100; i++){
+    if(Math.ceil(i/2) === i/2){
+        at = at + i
+    }
+}
+console.log(at)
+
+
+//Найдите сумму всех целых нечетных чисел в промежутке от 1 до 100.
+let ae = 0
+for(let i = 1; i<=100; i++){
+    if(Math.ceil(i/2) > i/2)
+    {ae = ae + i}
+}
+console.log(ae)
+
+
+//Даны два целых числа. 
+//Найдите остаток от деления первого числа на второе.
+const oneNumber = 43
+const twoNumber = 32
+let endOneTwo
+const oneTwo = (one, two) => {endOneTwo = (one/two).toString().split('.')
+    console.log(Number(endOneTwo[1]))
+}
+oneTwo(oneNumber, twoNumber) //34375
+
 
 
 
